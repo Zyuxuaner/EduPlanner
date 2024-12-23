efbaf7716197052fb80a48a97b037f126e5ae648
zyx <zyx1478948@qq.com>
2024-12-23 18:38:32 +0800

    feat:初始化导航

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { NavComponent } from './layout/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LayoutComponent,
+    HeaderComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
