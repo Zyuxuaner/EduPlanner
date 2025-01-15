5c75c7cb25924c87cc4da07bfc02b432f23f883f
zyx <zyx1478948@qq.com>
2025-01-15 22:13:33 +0800

    feat:班级新增

---
+++ web/src/app/clazz/add/add.component.spec.ts
@@ -0,0 +1,33 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddComponent } from './add.component';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {SchoolSelectModule} from "../../core/school-select/school-select.module";
+import {ReactiveFormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
+describe('AddComponent', () => {
+  let component: AddComponent;
+  let fixture: ComponentFixture<AddComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AddComponent ],
+      imports: [
+        HttpClientTestingModule,
+        SchoolSelectModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule
+      ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AddComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
