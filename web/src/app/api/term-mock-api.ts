4b6b8f69fcd1bd53fbc820b3b007609ab47b652c
zyx <zyx1478948@qq.com>
2025-01-17 13:43:11 +0800

    fix

--- web/src/app/api/term-mock-api.ts
+++ web/src/app/api/term-mock-api.ts
@@ -1,5 +1,6 @@
 import {ApiInjector, MockApiInterface} from "@yunzhi/ng-mock-api";
 import {School} from "../entity/school";
+import {Term} from "../entity/term";
 
 export class TermMockApi implements MockApiInterface {
   getInjectors(): ApiInjector[] {
@@ -35,11 +36,11 @@ export class TermMockApi implements MockApiInterface {
         url: '/term/getTermAndWeeks/:schoolId',
         result: (urlMatcher: any) => {
           const schoolId = parseInt(urlMatcher.schoolId);
-          const weeksData: Record<number, { status: boolean; messeage: string, data: { weeks: number[], term: string}}> = {
-            1: {status: true, messeage: '成功', data: {weeks: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16], term: '2024年秋'}},
-            2: {status: true, messeage: '成功', data: {weeks: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18], term: '2024年秋'}},
-            3: {status: true, messeage: '成功', data: {weeks: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 ,18, 19, 20], term: '2024年秋'}},
-            4: {status: true, messeage: '成功', data: {weeks: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20], term: '2024年秋'}},
+          const weeksData: Record<number, { status: boolean; messeage: string, data: { weeks: number[], term: {}}}> = {
+            1: {status: true, messeage: '成功', data: {weeks: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16], term: {name:'天职师大2024年秋'}}},
+            2: {status: true, messeage: '成功', data: {weeks: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18], term: {name: '河工大2024年秋'}}},
+            3: {status: true, messeage: '成功', data: {weeks: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 ,18, 19, 20], term: {name: '河师大2024年秋'}}},
+            4: {status: true, messeage: '成功', data: {weeks: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20], term: {name: '海河大学2024年秋'}}},
           };
           return weeksData[schoolId] || {status: false, messeage: '计算失败，不存在激活学期'};
         }
