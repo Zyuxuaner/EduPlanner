c4948e4529ca2bd90034733703d7b6a12ae133c8
zyx <zyx1478948@qq.com>
2025-01-20 22:12:08 +0800

    feat:初始化API（后台）

--- api/src/test/java/com/mengyunzhi/eduPlanner/EduPlannerApplicationTests.java
+++ api/src/test/java/com/mengyunzhi/eduPlanner/EduPlannerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.mengyunzhi.springBootStudy;
+package com.mengyunzhi.eduPlanner;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootStudyApplicationTests {
+public class EduPlannerApplicationTests {
 
 	@Test
 	public void contextLoads() {
