227402405f66c1d7c5f838d97a53ae776fb1b474
zyx <zyx1478948@qq.com>
2025-02-19 00:41:00 +0800

    fix:unit-testing

--- web/src/app/course/course.component.spec.ts
+++ web/src/app/course/course.component.spec.ts
@@ -3,9 +3,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CourseComponent } from './course.component';
 import {HttpClientTestingModule} from "@angular/common/http/testing";
 import {ReactiveFormsModule} from "@angular/forms";
-import {NzAffixModule} from "ng-zorro-antd/affix";
 import {NzSelectModule} from "ng-zorro-antd/select";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {WeekRangeFormatPipe, WeekTypeFormatPipe} from "../pipe/course-pipes";
 
 describe('CourseComponent', () => {
   let component: CourseComponent;
@@ -18,7 +18,9 @@ describe('CourseComponent', () => {
         HttpClientTestingModule,
         ReactiveFormsModule,
         NzSelectModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        WeekRangeFormatPipe,
+        WeekTypeFormatPipe
       ]
     })
     .compileComponents();
